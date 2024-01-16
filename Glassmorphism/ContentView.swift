15d6c7b73d32204f0513f012602af7bfd3899d48
ByoungYup <20170926@vision.hoseo.edu>
2024-01-16 17:16:39 +0900

    commit

--- Glassmorphism/ContentView.swift
+++ Glassmorphism/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.white, .white.opacity(0.0)]), startPoint: .top, endPoint: .bottom)
+                
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)).opacity(0.4), Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.6)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                
+                ZStack {
+                    Circle()
+                        .frame(width: 400, height: 400)
+                        .offset(x: 150, y: -200)
+                        .foregroundStyle(Color.blue.opacity(0.5))
+                        .blur(radius: 8)
+                    
+                    Circle()
+                        .frame(width: 400, height: 400)
+                        .offset(x: -150, y: 200)
+                        .foregroundStyle(Color.green.opacity(0.5))
+                        .blur(radius: 8)
+                    
+                    PopupView(isPresented: $isPresented)
+                }
+            }
+            .ignoresSafeArea()
+            .navigationDestination(for: Color.self) {
+                Color($0)
+            }
+        }
+    }
+}
+
+struct PopupView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        ZStack {
+            Color.white.opacity(0.35)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 10)
+            .blur(radius: 1)
+            
+            VStack {
+                
+                    Text("Glassmorphism")
+                        .font(.system(size: 24, weight: .bold))
+                
+                NavigationLink("Push", value: Color.white)
+
+            }
         }
         .padding()
+        .frame(width: 300, height: 400)
+        .foregroundStyle(Color.black.opacity(0.8))
     }
 }
 
