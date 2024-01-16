085bacca5ebdb7ee834f9a5f2c57e9bae69de084
ByoungYup <20170926@vision.hoseo.edu>
2024-01-16 16:44:04 +0900

    Initial Commit

---
+++ Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by 김병엽 on 2024/01/16.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
